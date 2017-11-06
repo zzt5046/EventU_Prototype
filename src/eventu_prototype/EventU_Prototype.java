b584c71dfb805abe0522c40102a2f9cf1f6f3b8e
Zach Thomas <zzt5046@psu.edu>
2017-11-06 17:00:25 -0500

    sorry

--- src/eventu_prototype/EventU_Prototype.java
+++ src/eventu_prototype/EventU_Prototype.java
@@ -15,7 +15,7 @@ public class EventU_Prototype {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
